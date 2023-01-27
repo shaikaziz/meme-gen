b7cfbf1e8d139d6342c387333645cbe006412332
shaik azizuddin <shaikazi@buffalo.edu>
2023-01-26 21:51:58 -0500

    css changes

--- src/Header.js
+++ src/Header.js
@@ -4,11 +4,10 @@ const Header = () => {
     return (
         <header className="header">
             <img 
-                src="./images/troll-face.png" 
+                src={log} 
                 className="header--image"
             />
-            <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
+            <h2 className="header--title">Make your memes</h2>
         </header>
     )
 }
